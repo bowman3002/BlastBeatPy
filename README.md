a427444058e8d5fbf03c953800a9a71a2ba0f7b6
Andrew Lang <andrew.lang19@gmail.com>
2019-05-17 10:12:30 -0400

    Setup for basic blast beat

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BlastBeatPy
+
+For virtual env setup:
+https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+
+
---
+++ dev/BlastBeat.py
@@ -0,0 +1,20 @@
+from midiutil import MIDIFile
+
+track    = 0
+channel  = 9
+time     = 0    # In beats
+duration = 1    # In beats
+tempo    = 60   # In BPM
+volume   = 100  # 0-127, as per the MIDI standard
+
+MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
+                      # automatically)
+MyMIDI.addTrackName(track, 0, "Bass")
+MyMIDI.addProgramChange(track, 9, 0, 30)
+MyMIDI.addTempo(track, time, tempo)
+
+for i in range(33, 81):
+    MyMIDI.addNote(track, channel, i, time + i, duration, volume)
+
+with open("major-scale.mid", "wb") as output_file:
+    MyMIDI.writeFile(output_file)
